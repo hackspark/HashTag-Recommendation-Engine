2519db11a53c8311d5e7823718bab3334ea8a0ff
CLQ6079 <guangyuliu@Guangyus-MacBook-Pro.local>
2015-06-14 10:08:45 -0700

    jurassic project

---
+++ src/main/java/com/twitter/utils/Tester.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.twitter.utils;
+
+/**
+ *
+ * @author gualiu
+ */
+public class Tester {
+  
+  public static void main(String args[]) {
+    
+    
+    String word = "asdf.";
+    
+    //if(word.matches("[.,:;-!]"))
+    
+    
+    
+  }
+  
+  
+}
